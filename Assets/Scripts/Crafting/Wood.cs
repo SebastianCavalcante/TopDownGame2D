51db34470b4f0266602dd3151f6b4a70caf083b3
SebastianCavalcante <sebahcavalcante073@gmail.com>
2025-10-22 10:37:31 -0300

    Update Wood.cs

--- Assets/Scripts/Crafting/Wood.cs
+++ Assets/Scripts/Crafting/Wood.cs
@@ -2,7 +2,27 @@ using UnityEngine;
 
 public class Wood : MonoBehaviour
 {
-    private int woodValue = 1;
+    [Header("Wood Settings")]
+    [SerializeField] private int woodValue = 1;
+    [SerializeField] private float slideSpeed;
+    [SerializeField] private float totalDistance;
+    private float slideDistance;
+
+
+    private void Update()
+    {
+        SlideWood();
+    }
+
+    private void SlideWood()
+    {
+        slideDistance += Time.deltaTime;
+
+        if (slideDistance < totalDistance)
+        {
+            transform.Translate(Vector2.left * slideSpeed * Time.deltaTime);
+        }
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
